2bb6d6de2d882fd9fadb31e5ef057e54029bf51a
andreu112 <anbelpel@upv.es>
2023-05-08 19:00:18 +0200

    Clean commit of the amte node

---
+++ amte-dataports/README.md
@@ -0,0 +1,30 @@
+The node AMTE_NODE_RED provides information from DataPorts Automatic Model Training Engine.
+
+# Installation
+
+Requirements:
+
+- node-red
+
+Steps:
+
+Copy folder `node-red-amte-dataports` in the node_modules directory (Windows `C:\Users\%username%\.node-red\node_modules` or linux `~/.node-red/node_modules`) and install the dependencies:
+
+```sh
+cp ./node-red-amte-dataports PATH/TO/NODE_MODULES
+```
+
+```sh
+... node_modules directory ...
+cd node-red-amte-dataports
+npm install
+```
+
+# License
+
+ASL 2.0 license
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priv
---
+++ amte-dataports/list-services/list-services.js
@@ -0,0 +1,93 @@
+/**
+ * DataPorts - A Data Platform for the Connection of Cognitive Ports
+ * This project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement No 871493
+ *
+ * Copyright (C) 2020-2023, by (Author's company of this file):
+ * - ITI – Instituto Tecnológico de Informática - Project Coordinator
+ * - UNIVERSITAT POLITÈCNICA DE VALÈNCIA (UPV)
+ *
+ * For more information, contact:
+ * Project coordinator:  <a href="mailto:info@dataports-project.eu"></a>
+ *
+ *
+ *    This code is licensed under the ASL 2.0 license, available at the root
+ *    application directory.
+ */
+
+
+const axios = require('axios');
+
+module.exports = function(RED) {
+    function ListServicesNode(config) {
+        RED.nodes.createNode(this,config);
+
+        var node = this;
+
+        // Get the parameters introduced by the user on the Frontend
+        this.username = config.username;
+        this.password = config.password;
+        
+        this.on('input', function(msg) {
+            
+            // Function to request the Token from Keycloack 
+
+            async function getTokenKeycloack() {
+                try {
+                    const response = await axios({
+                        method: 'POST',
+                        url: 'https://iam.dataports.com.es:8443/auth/realms/DataPorts/protocol/openid-connect/token',
+                        headers: {
+                            'Accept': ['application/json', 'text/plain', '*/*'],
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Authorization': 'Basic QXV0b21hdGljTW9kZWxzVHJhaW5pbmdFbmdpbmUyOmxIYndacDR2V2VpMkhNd2RBSkpiVlNjQmVpSHFUYWpB'
+                           },
+                        data: {
+                            'username': node.username,
+                            'password': node.password,
+                            'grant_type': 'password'
+                        }
+                    });
+                    
+                    return response.data.access_token
+
+                } catch (err) {
+                    console.error(err);
+                }
+            };
+            
+
+            // Function to request the Services List from AMTE 
+
+            async function getServicesList(token) {
+                try {
+                    const response = await axios({
+                        method: 'GET',
+                        url: 'http://amte.dataports-project.eu/api/services',
+                        headers: {
+                            'Authorization': 'Bearer ' + token
+                           },
+                    });
+                    
+                    return response.data
+
+                } catch (err) {
+                    console.error(err);
+                }
+            };
+
+
+            // MAIN 
+
+            getTokenKeycloack().then(token => {
+                getServicesList(token).then(servicesList => {
+                    console.log(servicesList)
+                    msg.payload = JSON.stringify(servicesList)
+                    this.send(msg)
+                })    
+            })
+        });
+    }
+    RED.nodes.registerType("list-services",ListServicesNode);
+}
+
+
